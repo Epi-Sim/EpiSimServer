c647008b055768c0e9bb6a26ac039949ae6ca527
lknoxstr <lknoxstr@bsc.es>
2024-09-02 14:44:16 +0200

    db update along with params saving

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -1,8 +1,16 @@
+CREATE TABLE IF NOT EXISTS simulation_params (
+    id TEXT PRIMARY KEY,
+    params BLOB,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS simulation_results (
     id TEXT PRIMARY KEY,
-    output BLOB,
+    file_path TEXT NOT NULL,
+    params_hash TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (params_hash) REFERENCES simulation_params(id)
 );
 
 CREATE TRIGGER IF NOT EXISTS update_simulation_results_timestamp
